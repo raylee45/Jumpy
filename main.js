4298823f6c5344e70d0c6ab79b05a1762ea209c3
raylee45 <raylee45@gmail.com>
2022-07-23 17:32:20 -0700

    background update

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const gameDisplay = document.querySelector('game-container');
     const ground = document.querySelector('.ground');
 
-    let birdLeft = 220;
-    letbirdBottom = 100;
+    let birdLeft = 220; // adds space between bird and the sky from left side
+    let birdBottom = 100; // adds space between bird and bottom of sky.
 
     function startGame() {
         bird.style.bottom = birdBottom + 'px'
