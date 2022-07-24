3fbc80bef866e892f775700fe7d64e21e25bc8b3
Ray Lee <107227057+raylee45@users.noreply.github.com>
2022-07-23 17:25:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Project 1");
+
+//select:
+const sky = document.getElementsByClassName('sky');
+
+//EventListeners:
+document.addEventListener('DOMContentLoaded', () => {
+    const bird = documentquerySelector('.bird');
+    const gameDisplay = document.querySelector('game-container');
+    const ground = document.querySelector('.ground');
+
+    let birdLeft = 220;
+    letbirdBottom = 100;
+
+    function startGame() {
+        bird.style.bottom = birdBottom + 'px'
+    }
+})
