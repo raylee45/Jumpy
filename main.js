b820f385804a0ac120059406c6c6e93f98e6ad8c
raylee45 <raylee45@gmail.com>
2022-07-26 22:26:42 -0700

    added top obstacle

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let birdBottom = 100
     let gravity = 2
     let isGameOver = false
+    let gap = 250
 
     function startGame() {
         birdBottom -= gravity
@@ -34,10 +35,17 @@ document.addEventListener('DOMContentLoaded', () => {
         let randomHeight = Math.random() * 60
         let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
-        if (!isGameOver) obstacle.classList.add('obstacle')
+        const topObstacle = document.createElement('div')
+        if (!isGameOver) {
+            obstacle.classList.add('obstacle')
+            topObstacle.classList.add('topObstacle')
+        }
         gameDisplay.appendChild(obstacle)
+        gameDisplay.appendChild(topObstacle)
         obstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
         obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
 
         function moveObstacle() {
             obstacleLeft -= 2
