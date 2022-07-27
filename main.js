91c926196e7817aac8d7133755d5dde8e60e993e
raylee45 <raylee45@gmail.com>
2022-07-26 19:42:29 -0700

    added first obstacle

--- main.js
+++ main.js
@@ -15,11 +15,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     let timerId = setInterval(startGame, 20)
 
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+    }
+
     function jump() {
-        birdBottom += 50
+        if (birdBottom < 500) birdBottom += 50
         bird.style.bottom = birdBottom + 'px'
         console.log(birdBottom)
     }
-    document.addEventListener('keyup', jump)
+    document.addEventListener('keyup', control)
     
+    function generateObstacle() {
+        let obstacleLeft = 500
+        let obstacleBottom = 150
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+    }
+    generateObstacle()
+
 })
